59aff48aff4d6ffabbaaede22c35c3f765d029cb
Gabriel Martins <gabrielmartinsdesouzaa@hotmail.com>
2022-10-18 20:18:53 -0300

    criação de teste com jest para funções de folha de pagamento

--- index.js
+++ index.js
@@ -1,39 +1,8 @@
-import assert from 'node:assert/strict';
-
-/* eslint-disable no-console */
 const somaHorasExtras = (salario, valorHorasExtra) => salario + valorHorasExtra;
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const verifiqueSe = (valor) => {
-  const assercoes = {
-    ehExatamentoIgualA(esperado) {
-      assert.strictEqual(valor, esperado);
-    },
-  };
-
-  return assercoes;
+export {
+  somaHorasExtras,
+  calculaDescontos,
 };
-
-const teste = (titulo, funcaoDeTeste) => {
-  try {
-    funcaoDeTeste();
-    console.log(`${titulo} passou!`);
-  } catch (error) {
-    console.error(`${titulo} não passou!`);
-  }
-};
-
-teste('somaHorasExtras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
-
-  verifiqueSe(retornado).ehExatamentoIgualA(esperado);
-});
-
-teste('caculaDesconto', () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 200);
-
-  verifiqueSe(retornado).ehExatamentoIgualA(esperado);
-});
