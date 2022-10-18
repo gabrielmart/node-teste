fea625b4400b99c091a486c924d127b9661f20fe
Gabriel Martins <gabrielmartinsdesouzaa@hotmail.com>
2022-10-18 19:41:20 -0300

    Adição de type module no package.json e uso do assert para realizar teste nativamente no Node

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
+import assert from 'node:assert/strict';
+
+/* eslint-disable no-console */
 const somaHorasExtras = (salario, valorHorasExtra) => salario + valorHorasExtra;
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
-    console.log(`${titulo} passou`);
-  } else {
-    console.error(`${titulo} não passou`);
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamentoIgualA(esperado) {
+      assert.strictEqual(valor, esperado);
+    },
+  };
+
+  return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`${titulo} passou!`);
+  } catch (error) {
+    console.error(`${titulo} não passou!`);
   }
 };
 
-teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+teste('somaHorasExtras', () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
 
-teste('caculaDesconto', 2200, calculaDescontos(2500, 300));
+  verifiqueSe(retornado).ehExatamentoIgualA(esperado);
+});
+
+teste('caculaDesconto', () => {
+  const esperado = 2300;
+  const retornado = calculaDescontos(2500, 200);
+
+  verifiqueSe(retornado).ehExatamentoIgualA(esperado);
+});
